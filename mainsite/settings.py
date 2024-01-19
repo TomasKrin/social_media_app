86b9315c6a68d14fe60151312821be07ac7fb257
Tomas Krinickas <tomas.krinickas2@gmail.com>
2024-01-19 14:58:07 +0200

    connected new packages with the app

--- mainsite/settings.py
+++ mainsite/settings.py
@@ -15,7 +15,6 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
@@ -27,13 +26,15 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-
 # Application definition
 
 INSTALLED_APPS = [
     'social_media',
     'polls',
     'chat',
+    'tinymce',
+    'crispy_forms',
+    'crispy_bootstrap4',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -72,7 +73,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'mainsite.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
@@ -82,7 +82,6 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
@@ -102,7 +101,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
@@ -114,7 +112,6 @@ USE_I18N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
@@ -124,3 +121,33 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+TINYMCE_DEFAULT_CONFIG = {
+    'height': 360,
+    'width': 1120,
+    'cleanup_on_startup': True,
+    'custom_undo_redo_levels': 20,
+    'selector': 'textarea',
+    'plugins': '''
+ textcolor save link image media preview codesample contextmenu
+ table code lists fullscreen insertdatetime nonbreaking
+ contextmenu directionality searchreplace wordcount visualblocks
+ visualchars code fullscreen autolink lists charmap print hr
+ anchor pagebreak
+ ''',
+    'toolbar1': '''
+ fullscreen preview bold italic underline | fontselect,
+ fontsizeselect | forecolor backcolor | alignleft alignright |
+ aligncenter alignjustify | indent outdent | bullist numlist table |
+ | link image media | codesample |
+ ''',
+    'toolbar2': '''
+ visualblocks visualchars |
+ charmap hr pagebreak nonbreaking anchor | code |
+ ''',
+    'contextmenu': 'formats | link image',
+    'menubar': True,
+    'statusbar': True,
+}
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
