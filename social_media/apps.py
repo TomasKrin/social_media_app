e3e7a4ea465f455a1f39ace1a5ccf1e52cf4cb74
Tomas Krinickas <tomas.krinickas2@gmail.com>
2024-01-23 21:04:36 +0200

    implemented signal system, so every registered user gets profile created

--- social_media/apps.py
+++ social_media/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class SocialMediaConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'social_media'
+
+    def ready(self) -> None:
+        from .signals import create_profile
