e23e21bca8f5aa320afeef6698e52ce3dd176291
Tomas Krinickas <tomas.krinickas2@gmail.com>
2024-01-23 08:53:54 +0200

    added socialmedia page models, admin view, styled base template and socialmedia-index

--- social_media/admin.py
+++ social_media/admin.py
@@ -1,3 +1,36 @@
 from django.contrib import admin
 
-# Register your models here.
+from social_media.models import Profile, UserPost, PostComment
+
+
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ('user', 'display_name', 'profile_views', 'profile_pic')
+    list_filter = ('user', 'display_name',)
+    list_editable = ('display_name', 'profile_pic',)
+    search_fields = ('user', 'display_name',)
+    fieldsets = (
+        ('Profile', {'fields': ('display_name', 'profile_pic')}),
+    )
+
+
+class UserPostAdmin(admin.ModelAdmin):
+    list_display = ('id', 'date_posted', 'profile', 'post',)
+    list_filter = ('date_posted', 'profile',)
+    search_fields = ('profile__display_name', 'profile__user__username')
+    fieldsets = (
+        ('Post', {'fields': ('post', 'img')}),
+    )
+
+
+class PostCommentAdmin(admin.ModelAdmin):
+    list_display = ('date_posted', 'get_post_id', 'profile', 'comment',)
+    list_filter = ('date_posted', 'profile',)
+    search_fields = ('profile__display_name', 'profile__user__username')
+    fieldsets = (
+        ('Comment', {'fields': ('comment',)}),
+    )
+
+
+admin.site.register(Profile, ProfileAdmin)
+admin.site.register(UserPost, UserPostAdmin)
+admin.site.register(PostComment, PostCommentAdmin)
--- social_media/models.py
+++ social_media/models.py
@@ -1,3 +1,47 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    display_name = models.CharField(max_length=50)
+    profile_views = models.IntegerField(default=0)
+    profile_pic = models.ImageField('profile_pic', default='profile_pic/default.png', upload_to='profile_pics')
+
+    def __str__(self):
+        return self.display_name
+
+
+class UserPost(models.Model):
+    date_posted = models.DateTimeField(auto_now_add=True)
+    profile = models.ForeignKey(Profile, on_delete=models.SET_NULL, null=True, blank=True)
+    post = models.TextField(verbose_name='Post')
+    img = models.ImageField('Image', null=True, blank=True)
+
+    def __str__(self):
+        return f'{self.date_posted} {self.profile} {self.post}'
+
+
+class PostComment(models.Model):
+    date_posted = models.DateTimeField(auto_now_add=True)
+    profile = models.ForeignKey(Profile, on_delete=models.SET_NULL, null=True, blank=True)
+    post = models.ForeignKey(UserPost, on_delete=models.CASCADE)
+    comment = models.TextField(verbose_name='Comment')
+
+    def get_post_id(self):
+        return self.post.id
+
+    get_post_id.short_description = 'Post'
+
+    def __str__(self):
+        return f'{self.date_posted} {self.profile} {self.comment}'
+
+
+class PostLike(models.Model):
+    post = models.ForeignKey(UserPost, on_delete=models.CASCADE)
+    profile = models.ForeignKey(Profile, on_delete=models.CASCADE)
+
+
+class CommentLike(models.Model):
+    comment = models.ForeignKey(PostComment, on_delete=models.CASCADE)
+    profile = models.ForeignKey(Profile, on_delete=models.CASCADE, null=True, blank=True)
